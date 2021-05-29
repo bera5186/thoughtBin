f73863f7ba8604021d2fed9e8786507e379973fb
bera5186 <rbasu611@gmail.com>
2021-05-29 22:13:09 +0530

    Feat: Added Post and Comment Model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+import uuid
+from authMiddleware.models import User
 
 # Create your models here.
+class Post(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=400, db_index=True, null=False)
+    text = models.TextField(max_length=6000, null=False)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    slug = models.CharField(max_length=400, unique=True, db_index=True, default=uuid.uuid4)
+    private = models.BooleanField(default=False)
+    likes = models.BigIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.slug
+
+class Comment(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    comment = models.TextField(max_length=1000, null=False)
+    post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    author_reply = models.TextField(max_length=1000, default=None, null=True, blank=True)
+    is_liked_by_author = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return str(self.id)
